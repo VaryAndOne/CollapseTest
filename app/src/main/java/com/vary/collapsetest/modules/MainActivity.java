3902416a30a5bf5eaacd5dec88b53e190f7b4530
Vary <VaryAndOne@163.com>
2017-04-19 11:03:19 +0800

    Test

---
+++ app/src/main/java/com/vary/collapsetest/modules/MainActivity.java
@@ -0,0 +1,15 @@
+package com.vary.collapsetest.modules;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vary.collapsetest.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
