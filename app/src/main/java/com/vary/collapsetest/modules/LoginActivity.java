3ef393d1ff434883dbe032b0ee62e5b4c767ef2c
vary <VaryAndOne@163.com>
2017-04-24 17:06:48 +0800

    login

---
+++ app/src/main/java/com/vary/collapsetest/modules/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.vary.collapsetest.modules;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vary.collapsetest.R;
+
+/**
+ * Created by Administrator on 2017-04-24.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
