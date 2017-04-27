3bc1034fbd82bd2bfab8a98cb3a579372271d780
vary <VaryAndOne@163.com>
2017-04-27 16:40:29 +0800

    Media

--- app/src/main/java/com/vary/collapsetest/modules/MediaActivity.java
+++ app/src/main/java/com/vary/collapsetest/modules/MediaActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.vary.collapsetest.R;
+import com.vary.collapsetest.modules.fragment.CameraFragment;
 
 /**
  * Created by Administrator on 2017-04-26.
@@ -16,5 +17,10 @@ public class MediaActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_media);
+
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new CameraFragment()).commit();
+        }
     }
 }
