1049aceb48472edf814d2b5172f7829f158dfe17
vary <VaryAndOne@163.com>
2017-05-21 12:02:59 +0800

    shape_task

--- app/src/main/java/com/vary/collapsetest/mvp/model/Shop.java
+++ app/src/main/java/com/vary/collapsetest/mvp/model/Shop.java
@@ -29,12 +29,12 @@ public class Shop {
 
     public List<Item> getData() {
         return Arrays.asList(
-                new Item(1, "Everyday Candle", "$12.00 USD", R.drawable.shop1),
-                new Item(2, "Small Porcelain Bowl", "$50.00 USD", R.drawable.shop2),
-                new Item(3, "Favourite Board", "$265.00 USD", R.drawable.shop3),
-                new Item(4, "Earthenware Bowl", "$18.00 USD", R.drawable.shop4),
-                new Item(5, "Porcelain Dessert Plate", "$36.00 USD", R.drawable.shop5),
-                new Item(6, "Detailed Rolling Pin", "$145.00 USD", R.drawable.shop6));
+                new Item(1, "Everyday Candle", "12.00", R.drawable.shop1),
+                new Item(2, "Small Porcelain Bowl", "50.00", R.drawable.shop2),
+                new Item(3, "Favourite Board", "265.00", R.drawable.shop3),
+                new Item(4, "Earthenware Bowl", "18.00", R.drawable.shop4),
+                new Item(5, "Porcelain Dessert Plate", "36.00", R.drawable.shop5),
+                new Item(6, "Detailed Rolling Pin", "145.00", R.drawable.shop6));
     }
 
     public boolean isRated(int itemId) {
