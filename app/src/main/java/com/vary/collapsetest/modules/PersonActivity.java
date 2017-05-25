86df3f9c6ab854774e03116b3da345a78568b596
vary <VaryAndOne@163.com>
2017-05-25 10:23:49 +0800

    head_change

--- app/src/main/java/com/vary/collapsetest/modules/PersonActivity.java
+++ app/src/main/java/com/vary/collapsetest/modules/PersonActivity.java
@@ -50,7 +50,7 @@ public class PersonActivity extends AppCompatActivity {
         List<Person> persons = new ArrayList<>();
         persons.add(new Person("任务",R.drawable.icon_app));
         persons.add(new Person("群组",R.drawable.ic_group_black_24dp));
-        persons.add(new Person("收藏",R.drawable.ic_collections_black_24dp));
+        persons.add(new Person("盯紧",R.drawable.ic_flag_black_24dp));
         persons.add(new Person("我的",R.drawable.ic_camera_alt_black_24dp));
         persons.add(new Person("清除缓存",R.drawable.ic_delete_sweep_black_24dp));
         persons.add(new Person("设置",R.drawable.ic_settings_black_24dp));
