c25d19c208f74d0338c74e36e5f28b06a4de927a
vary <VaryAndOne@163.com>
2017-05-26 10:17:48 +0800

    camera

---
+++ app/src/main/java/com/vary/collapsetest/modules/CameraActivity.java
@@ -0,0 +1,20 @@
+package com.vary.collapsetest.modules;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.vary.collapsetest.R;
+
+/**
+ * Created by Administrator on 2017-05-26.
+ */
+
+public class CameraActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
